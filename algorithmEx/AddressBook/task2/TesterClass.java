d09eab4c9adcc02f27f3714618fe5931c0048e4a
dayoung-lee <55811738+dayoung-lee@users.noreply.github.com>
2019-10-03 19:00:44 -0400

    Add files via upload

---
+++ algorithmEx/AddressBook/task2/TesterClass.java
@@ -0,0 +1,20 @@
+package task2;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import java.io.*;
+
+public class TesterClass {
+	protected AddressBook adb = new AddressBook();
+	final int FNAME = 32;
+	final int LNAME = 32;
+	final int CITY = 20;
+	final int ZIP = 6;
+	protected int count = 0;
+	
+	public void start(Stage primaryStage) {
+		
+	}
+}
